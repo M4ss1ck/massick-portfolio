5a5fb8204ed32459b3480eaa972950852fde268c
M4ss1ck <4ndyraul@gmail.com>
2022-04-25 19:12:20 -0400

    rename Comments.tsx

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react"
 import { Trans } from "gatsby-plugin-react-i18next"
 //import ThemeContext from '../context/ThemeContext';
+
 export default class Comments extends Component {
   //static contextType = ThemeContext;
-
-  constructor(props) {
+  commentBox: React.RefObject<HTMLDivElement>
+  constructor(props: any) {
     super(props)
-    this.commentBox = React.createRef() // Creates a reference to inject the <script> element
+    this.commentBox = React.createRef()
   }
   componentDidMount() {
     //const theme = this.context;
@@ -15,18 +16,20 @@ export default class Comments extends Component {
     let scriptEl = document.createElement("script")
     scriptEl.setAttribute("src", "https://utteranc.es/client.js")
     scriptEl.setAttribute("crossorigin", "anonymous")
-    scriptEl.setAttribute("async", true)
+    scriptEl.setAttribute("async", "true")
     scriptEl.setAttribute("repo", "M4ss1ck/portfolio-comments")
     scriptEl.setAttribute("issue-term", "og:title")
     scriptEl.setAttribute("theme", utteranceTheme)
-    this.commentBox.current.appendChild(scriptEl)
+    if (this.commentBox.current) {
+      this.commentBox.current.appendChild(scriptEl)
+    }
   }
   // TODO: Add a way to change the theme of the comments
 
   render() {
     return (
       <div className="prose comment-box-wrapper pt-7 dark:prose-dark">
-        <h1 className="mb-0 text-lg  text-primario dark:text-secundario font-merriweather">
+        <h1 className="mb-0 text-lg text-primario dark:text-secundario font-merriweather">
           <Trans>Comments</Trans>
         </h1>
         <hr className="my-0" />
