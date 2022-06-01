5973e3be42b6e7ba016f52c3cc08745d1bfa4ba3
M4ss1ck <4ndyraul@gmail.com>
2022-06-01 01:00:21 -0400

    fix: darker gray for language switcher

--- src/components/LanguageSwitcher/Language.tsx
+++ src/components/LanguageSwitcher/Language.tsx
@@ -12,7 +12,7 @@ const Language = () => {
             className={
               language === lng
                 ? "text-center text-sm lg:text-lg py-4 font-montserrat cursor-pointer outline outline-primario dark:outline-secundario outline-1 text-primario dark:text-secundario  inline-flex items-center px-2 pt-2 pb-2 font-medium leading-5 transition duration-250 ease-in-out mx-2 rounded-lg"
-                : "text-center text-sm lg:text-lg py-4 font-montserrat cursor-pointer border-transparent text-gray-500 dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-200 hover:outline-grey-600 focus:text-gray-600 focus:outline-grey-600 inline-flex items-center px-1 pt-2 pb-2 border-b-4 font-medium leading-5 focus:outline-none transition duration-250 ease-in-out mx-2 rounded-lg"
+                : "text-center text-sm lg:text-lg py-4 font-montserrat cursor-pointer border-transparent text-gray-600 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 hover:outline-grey-600 focus:text-gray-600 focus:outline-grey-600 inline-flex items-center px-1 pt-2 pb-2 border-b-4 font-medium leading-5 focus:outline-none transition duration-250 ease-in-out mx-2 rounded-lg"
             }
           >
             <Link to={originalPath} language={lng}>
